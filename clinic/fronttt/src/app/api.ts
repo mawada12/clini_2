de7398c4916f2b7bdc4a332f9a942b76e7f61a32
mawada12 <119057614+mawada12@users.noreply.github.com>
2023-12-28 21:00:23 +0200

    Delete clinic directory

--- clinic/fronttt/src/app/api.ts
+++
@@ -1,4 +0,0 @@
-export class ApiData {
-    static baseUrl = "https://backend-mawada12-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/";
-}
-  
--- clinic/fronttt/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.ts
-import Config from "../config.json";
-
-export const environment = {
-    production: false,
-    apiUrl: Config.API_URL,
-};
