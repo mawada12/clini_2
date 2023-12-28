f8772e8ec1a62c80f9d4b636a7de7c021159280c
mawada12 <119057614+mawada12@users.noreply.github.com>
2023-12-28 18:00:00 +0200

    Delete clinic directory

--- clinic/src/modules/user/user.api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { signUp, signIn } = require("./user.auth");
-const router = express.Router();
-
-router.route("/sign-up").post(signUp);
-router.route("/sign-in").post(signIn)
-
-module.exports = router;
